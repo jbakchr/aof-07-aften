b066bd8eea4f78e8691c4dc701aa8d5d9d060b56
Jonas Bak Phillipson <jbakchr@gmail.com>
2024-11-14 15:09:05 +0100

    Added exercise 4

---
+++ 02-exercises/04-print-odds-only/main.py
@@ -0,0 +1,8 @@
+"""
+Skriv et program, der kun udskriver de ulige tal fra nedenstående liste med tal. Brug en for-løkke og fortsæt med at springe over lige tal.
+
+Læg mærke til, at outputtet af dit program skal være følgende (dvs. et output på kun én linje!):
+1 3 5 7 9
+"""
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
